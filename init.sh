0c86e70020f7e5030c66032968aedf6ce7b1a5d1
8CD <mail@8cd.ru>
2016-11-11 18:34:03 +0300

    change init

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo rm -rf /etc/gunicorn.d/gunicorn/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
